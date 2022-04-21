d1d1985664160b849651ca2d57ffb70c3e52872f
artapamudaid <pamuda.id@gmail.com>
2022-04-21 11:09:46 +0700

    make livewire controller and route

--- app/Http/Livewire/PostCrud.php
+++ app/Http/Livewire/PostCrud.php
@@ -2,12 +2,76 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class PostCrud extends Component
 {
+
+    public $posts, $title, $desc, $post_id;
+    public $isModalOpen = 0;
+
     public function render()
     {
+        $this->posts = Post::all();
         return view('livewire.post-crud');
+    }
+
+    public function create()
+    {
+        $this->resetCreateForm();
+        $this->openModal();
+    }
+
+    public function openModal()
+    {
+        $this->isModalOpen = true;
+    }
+
+    public function closeModal()
+    {
+        $this->isModalOpen = false;
+    }
+
+    private function resetCreateForm()
+    {
+        $this->title = '';
+        $this->body = '';
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'title' => 'required|max:50',
+            'body' => 'required',
+        ]);
+
+        Post::updateOrCreate([
+            'id' => $this->id
+        ], [
+            'title' => $this->title,
+            'body' => $this->body
+        ]);
+
+        session()->flash('message', $this->post_id ? 'Data updated successfully.' : 'Data added successfully.');
+
+        $this->closeModal();
+        $this->resetCreateForm();
+    }
+
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        $this->id = $id;
+        $this->title = $post->title;
+        $this->body = $post->body;
+
+        $this->openModal();
+    }
+
+    public function delete($id)
+    {
+        Post::find($id)->delete();
+        session()->flash('message', 'Data deleted successfully.');
     }
 }
